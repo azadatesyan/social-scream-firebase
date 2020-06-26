b67d6f54887b7b2b5480322acbeb09d5c7391199
Olivier <olivier.atesyan@ieseg.fr>
2020-06-26 14:17:17 +0200

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+	response.send("Hello from Firebase!");
+});
+
+exports.getScreams = functions.https.onRequest(async (req, res) => {
+	try {
+		let screams = [];
+		let screamsQuery = await admin.firestore().collection("screams").get();
+		screamsQuery.forEach((scream) => {
+			screams.push(scream.data());
+		});
+		return res.json(screams);
+	} catch (err) {
+		console.log(err);
+	}
+});
