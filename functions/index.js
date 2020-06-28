995de8bde0261ceb19f8b22806ca4aba4ef32c9e
Olivier <olivier.atesyan@ieseg.fr>
2020-06-28 15:09:51 +0200

    add authentication middleware

--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,32 @@ const { admin, firebase, db } = require('./config');
 const app = require('express')();
 const { isEmpty, isEmail } = require('./validations');
 
+const isAuth = async (req, res, next) => {
+	let idToken;
+	if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+		idToken = req.headers.authorization.split('Bearer ')[1];
+	} else {
+		console.log('Invalid auth token');
+		return res.status(403).json({
+			error: 'Unauthorized'
+		});
+	}
+	try {
+		const decodedToken = await admin.auth().verifyIdToken(idToken);
+		req.user = decodedToken;
+		const userSnapshot = await db.collection('users').where('userId', '==', req.user.uid).limit(1).get();
+		req.user.username = await userSnapshot.docs[0].data().username;
+		return next();
+	} catch (err) {
+		console.log('Error while verifying token', err);
+		return res.status(403).json(err);
+	}
+};
+
 app.get('/screams', async (req, res) => {
 	try {
 		let screams = [];
-		let screamsQuery = await db.collection('screams').get();
+		let screamsQuery = await db.collection('screams').orderBy('createdAt', 'desc').get();
 		screamsQuery.forEach((scream) => {
 			screams.push({
 				screamId: scream.id,
@@ -19,18 +41,14 @@ app.get('/screams', async (req, res) => {
 	}
 });
 
-app.post('/screams/new', async (req, res) => {
+app.post('/screams/new', isAuth, async (req, res) => {
 	try {
 		let screamToPush = {
 			text: req.body.text,
-			username: req.body.username,
+			username: req.user.username,
 			createdAt: admin.firestore.Timestamp.fromDate(new Date())
 		};
-		let createdScream = await admin
-			.firestore()
-			.collection('screams')
-			.orderBy('createdAt', 'desc')
-			.add(screamToPush);
+		const createdScream = await db.collection('screams').add(screamToPush);
 		res.status(200).json({
 			message: `Scream ${createdScream.id} created successfully`
 		});
@@ -85,6 +103,7 @@ app.post('/signup', async (req, res) => {
 			const token = await signupResponse.user.getIdToken();
 			const createdUser = {
 				email: newUser.email,
+				username: newUser.username,
 				userId: signupResponse.user.uid,
 				createdAt: admin.firestore.Timestamp.fromDate(new Date())
 			};
@@ -106,8 +125,33 @@ app.post('/signup', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-	const userCredentials = await firebase.auth().signInWithEmailAndPassword(req.email, req.password);
-	const userToken = await userCredentials.user.getIdToken();
+	let errors = {};
+	const user = {
+		email: req.body.email,
+		password: req.body.password
+	};
+
+	if (isEmpty(user.email)) errors.email = 'Email must not be empty';
+	if (isEmpty(user.password)) errors.password = 'Password must not be empty';
+
+	if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+	try {
+		const userCredentials = await firebase.auth().signInWithEmailAndPassword(user.email, user.password);
+		const userToken = await userCredentials.user.getIdToken();
+		res.json({ userToken });
+	} catch (err) {
+		console.log(err);
+		if (err.code === 'auth/wrong-password') {
+			return res.status(403).json({
+				general: 'Wrong credentials, please try again'
+			});
+		} else {
+			return res.status(500).json({
+				error: err.code
+			});
+		}
+	}
 });
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
